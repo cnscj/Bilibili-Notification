19fbab84b8631d701033655b35a45a472f164576
cnscj <cnscj@qq.com>
2022-03-23 14:27:39 +0800

    init project

---
+++ Bilibili-Notification/Main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import os
+import sys
+from Configs import WebhookConfig
+
+def main():
+    print(WebhookConfig.DINGDING_PUSH_WEBHOOK_URL)
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ Bilibili-Notification/Servers/MessagePush/DingDingRobot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#coding:utf-8
+
+class DingDingRobot:
+    def push(self,content):
+        print(content)
+
+    #加签
+    def _sign(self):
+        return
+        
