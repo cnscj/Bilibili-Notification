d2d99efcc4866d07b70b9c347fdd9819d73b628e
cnscj <cnscj@qq.com>
2022-12-20 15:50:34 +0800

    [fix]:将进程pid打印到日志

--- Bilibili-Notification/main.py
+++ Bilibili-Notification/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 #coding:utf-8
-
+import os
 from managers.service_manager import service_manager
 from servers import notification_poll_service
 from servers import dingding_push_service
 from servers import sleep_cpu_service
+from utils.logger import logger
+
 def main():
+    pid = os.getpid()
+    logger.info('【进程服务】: 当前进程pid:{pid}'.format(pid=pid))
+
     service_manager.register_service(notification_poll_service.NotificationPollService())
     service_manager.register_service(dingding_push_service.DingdingPushService())
     service_manager.register_service(sleep_cpu_service.SleepCpuService())
